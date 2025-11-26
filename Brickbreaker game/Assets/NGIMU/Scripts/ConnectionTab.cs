6e6329e512a386c8a24a585b941460aa5c59b15d
Albe <119892946+albe-crm@users.noreply.github.com>
2025-11-26 12:24:54 +0100

    from 26 lab session
    added the mini brickbreaker game version (need new version from jessica) and wifi connection, ready for unity code

--- Brickbreaker game/Assets/NGIMU/Scripts/ConnectionTab.cs
+++
@@ -1,4 +0,0 @@
-namespace NGIMU.Scripts
-{
-
-}
--- Brickbreaker game/Assets/Scripts/players/SaveID.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SaveID : MonoBehaviour
-{
-    public static int saveID;   // Numero di giocatore 
-
-}
